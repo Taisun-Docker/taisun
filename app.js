f2f47d205625e8a74fae428e2bc575edaf580ae5
thelamer <ryankuba@gmail.com>
2017-08-28 00:26:51 +0000

    #1 got an early version of the embedded guacamole VDI endpoint for running containers need to pull out some hard codes and get create delete up and running

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 // Main Node.js app
 
 //// Application Variables ////
+const crypto = require('crypto');
 var events = require('events');
+var ejs = require('ejs');
 var emitter = new events.EventEmitter();
 var express = require('express');
 var app = require('express')();
@@ -11,20 +13,66 @@ var io = require('socket.io')(http);
 var Docker = require('dockerode');
 var docker = new Docker({socketPath: '/var/run/docker.sock'});
 var exec = require('child_process').exec;
-var vncports = [];
-var guacports = [];
-for (var i = 5900; i <= 5999; i++) {vncports.push(i);}
-for (var i = 7900; i <= 7999; i++) {guacports.push(i);}
+
+///// Guac Websocket Tunnel ////
+const GuacamoleLite = require('guacamole-lite');
+const websocketOptions = {
+  port: 3000
+};
+const guacdOptions = {
+  host: "172.17.0.4",
+  port: 4822
+};
+const clientOptions = {
+  crypt: {
+    cypher: 'AES-256-CBC',
+    key: 'TaisunKYTaisunKYTaisunKYTaisunKY'
+  },
+  log: {
+    verbose: false
+  }
+};
+// Spinup the Guac websocket proxy on port 3000
+const guacServer = new GuacamoleLite(websocketOptions, guacdOptions, clientOptions);
+// Function needed to encrypt the token string
+const encrypt = (value) => {
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv(clientOptions.crypt.cypher, clientOptions.crypt.key, iv);
+  let crypted = cipher.update(JSON.stringify(value), 'utf8', 'base64');
+  crypted += cipher.final('base64');
+  const data = {
+    iv: iv.toString('base64'),
+    value: crypted
+  };
+  return new Buffer(JSON.stringify(data)).toString('base64');
+};
+// Proxy websocket requests using a path so the client can connect even behind firewalls or through a proxy
 
 
 ////// PATHS //////
 //// Main ////
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+  res.render(__dirname + '/views/containers.ejs');
+});
+//// VDI ////
+app.get("/vdi", function (req, res) {
+  res.render(__dirname + '/views/vdi.ejs');
 });
 //// Public JS and CSS ////
 app.use('/public', express.static(__dirname + '/public'));
-
+//// Embedded guac ////
+app.get("/desktop/:containerid", function (req, res) {
+  var container = docker.getContainer(req.params.containerid);
+  container.inspect(function (err, data) {
+    if (err){
+      res.send('container does not exist');
+    }
+    else{
+      var connectionstring = encrypt({"connection":{"type":"vnc","settings":{"hostname":data.NetworkSettings.IPAddress,"port":"5900"}}});
+      res.render(__dirname + '/views/guac.ejs', {token : connectionstring});
+    }
+  });
+});
 
 // Update the containers page
 emitter.on('updatepage', function(updatetable) {
@@ -48,6 +96,7 @@ io.on('connection', function(socket){
     // Select current active containers in docker and send it to the client
     emitter.emit('updatepage');
 });
+
 
 // Spin up application on port 80
 http.listen(80, function(){
--- public/js/taisun.js
+++ public/js/taisun.js
@@ -9,20 +9,90 @@ $(document).ready(function(){
   });
   // Whenever the stack list is updated rebuild the displayed table
   socket.on('updatepage', function(containers) {
-    // Empty Containers Table
-    $('#dockercontainers').empty();
+    var containertable = $('#dockercontainers').DataTable( {} );
     //Loop through the containers to build the table
     for (var container in containers){
       var info = containers[container];
       var ports = info.Ports;
-      $('#dockercontainers').append(
-        '<tr scope="row"><td>' + info.Names[0] + '</td>' +
-        '<td><a href="http://' + host + ':' + ports[0].PublicPort + '" target="_blank">' + ports[0].PublicPort + '</a> => ' + ports[0].PrivatePort + '</td>' + 
-        '<td>' +  info.Image + '</td>' +
-        '<td>' +  info.State + ' ' + info.Status + '</td>' + 
-        '<td>' + new Date( info.Created * 1e3).toISOString().slice(0,19) + '</td>' + 
-        '<td>' + info.Command + '</td></tr>'
-      );
+      containertable.row.add( 
+        [info.Names[0], 
+        '<a href="http://' + host + ':' + ports[0].PublicPort + '" target="_blank">' + ports[0].PublicPort + '</a> => ' + ports[0].PrivatePort,
+        info.Image, 
+        info.State, 
+        new Date( info.Created * 1e3).toISOString().slice(0,19),
+        info.Command] 
+      ).draw();
+    }
+    // Empty desktops Table
+    $('#desktops').empty();
+    for (var container in containers){
+      var info = containers[container];
+      var ports = info.Ports;
+      if (info.Names[0].indexOf("taisunvdi_") != -1 ){
+        $('#desktops').append(
+  					'<div class="card text-center" style="width: 10rem;">' +
+            '  <img class="card-img-top" src="/public/img/debian.png">' +
+            '  <div class="card-block">' +
+            '    <h4 class="card-title">' + info.Names[0].replace('/taisunvdi_','') +'</h4>' +
+            '    <div class="card-footer">' + 
+            '      <a href="/desktop/' + info.Id + '" target="_blank" class="btn btn-primary">Launch</a>' +
+            '    </div>' + 
+            '  </div>' +
+            '</div>'
+        );
+      }
     }
   });
 });
+
+
+// Grabbed from the admin template
+(function($) {
+  "use strict"; // Start of use strict
+  // Configure tooltips for collapsed side navigation
+  $('.navbar-sidenav [data-toggle="tooltip"]').tooltip({
+    template: '<div class="tooltip navbar-sidenav-tooltip" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>'
+  });
+  // Toggle the side navigation
+  $("#sidenavToggler").click(function(e) {
+    e.preventDefault();
+    $("body").toggleClass("sidenav-toggled");
+    $(".navbar-sidenav .nav-link-collapse").addClass("collapsed");
+    $(".navbar-sidenav .sidenav-second-level, .navbar-sidenav .sidenav-third-level").removeClass("show");
+  });
+  // Force the toggled class to be removed when a collapsible nav link is clicked
+  $(".navbar-sidenav .nav-link-collapse").click(function(e) {
+    e.preventDefault();
+    $("body").removeClass("sidenav-toggled");
+  });
+  // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
+  $('body.fixed-nav .navbar-sidenav, body.fixed-nav .sidenav-toggler, body.fixed-nav .navbar-collapse').on('mousewheel DOMMouseScroll', function(e) {
+    var e0 = e.originalEvent,
+      delta = e0.wheelDelta || -e0.detail;
+    this.scrollTop += (delta < 0 ? 1 : -1) * 30;
+    e.preventDefault();
+  });
+  // Scroll to top button appear
+  $(document).scroll(function() {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $('.scroll-to-top').fadeIn();
+    } else {
+      $('.scroll-to-top').fadeOut();
+    }
+  });
+  // Configure tooltips globally
+  $('[data-toggle="tooltip"]').tooltip();
+  // Smooth scrolling using jQuery easing
+  $(document).on('click', 'a.scroll-to-top', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: ($($anchor.attr('href')).offset().top)
+    }, 1000, 'easeInOutExpo');
+    event.preventDefault();
+  });
+  // Call the dataTables jQuery plugin
+  $(document).ready(function() {
+    $('#dataTable').DataTable();
+  });
+})(jQuery); // End of use strict
